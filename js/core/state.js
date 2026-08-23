7ce06962dcc585759dc869a76a82df83b8c65df2
blessings2008 <madalitsotambala2@gmail.com>
2026-08-23 11:39:36 +0200

    Delete js directory

--- js/core/state.js
+++
@@ -1,9 +0,0 @@
-// PocketVault shared application state.
-// Kept in one module so future feature modules share the same state object.
-export const state = {
-  user: null,
-  plan: "free",
-  planConfig: null,
-  goals: {},
-  notifications: [],
-};
